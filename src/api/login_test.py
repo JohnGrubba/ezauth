a8436ca80f1ef95deb0bf1f3d6a2364a9b955c21
John Grubba <nicjontrickshots@gmail.com>
2024-07-19 10:28:32 +0200

    Dockerfile Migrated Tests Renamed Configtemplate Modified

--- src/api/login_test.py
+++ src/api/login_test.py
@@ -1,29 +1,8 @@
-import pytest
-import datetime
-import bcrypt
-from tools import users_collection
 from fastapi.testclient import TestClient
 
 from .main import app
 
 client = TestClient(app)
-
-
-@pytest.fixture
-def fixtureuser() -> dict:
-    users_collection.find_one_and_delete({"email": "fixtureuser@gmail.com"})
-    hashed_pswd = bcrypt.hashpw("Kennwort1!".encode("utf-8"), bcrypt.gensalt(5)).decode(
-        "utf-8"
-    )
-    user_data = {
-        "password": hashed_pswd,
-        "email": "fixtureuser@gmail.com",
-        "username": "FixtureUser",
-        "createdAt": datetime.datetime.now(),
-    }
-    users_collection.insert_one(user_data)
-    user_data["password"] = "Kennwort1!"
-    return user_data
 
 
 # SUCCESSFUL TESTS
